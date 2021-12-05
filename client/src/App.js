dc9400a2f0a2816301700c01a35b55e11ce61b4e
Igor Busarov <igobusarov@yahoo.com>
2021-12-05 21:51:00 +0200

    Create components and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./components/HomeScreen";
+import ProductScreen from "./components/ProductScreen";
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello</h1>
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
