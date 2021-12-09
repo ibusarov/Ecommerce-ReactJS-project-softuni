e5a516b7d97e77b92ae16c19ed2a4a937f3ce18f
Igor Busarov <igobusarov@yahoo.com>
2021-12-09 21:34:58 +0200

    Adding MongoDB

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
-import products from "./data/products.js";
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddlewear.js";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -10,14 +14,17 @@ app.get("/", (req, res) => {
   res.send("api is running....");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
+app.use("/api/products", productRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
 });
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(5000, console.log(`Server is runnimg on port ${PORT}`));
