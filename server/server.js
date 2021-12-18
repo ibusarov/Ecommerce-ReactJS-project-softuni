70519f49e2d06bf249740acb1112b78f09da66d7
igobusarov@yahoo.com <igobusarov@yahoo.com>
2021-12-18 23:35:33 +0200

    Prepare for deployment

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
@@ -14,13 +17,28 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('api is running....')
-})
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+// app.get('/', (req, res) => {
+//   res.send('api is running....')
+// })
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
 app.use((req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`)
